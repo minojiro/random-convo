4164f2102ba9cd55d52554577dd2ae2725f06f18
minojiro <minojiro@users.noreply.github.com>
2023-01-30 19:35:39 +0900

    feat: Homeページ

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    Home画面
+  <div class="h-screen w-screen flex justify-center items-center">
+    <div class="max-w-lg mx-auto my-10">
+      <h1 class="font-bold text-5xl text-center">RandomConvo</h1>
+      <p class="my-5">ランダムマッチチャットサービスは、世界中の誰かと瞬時にチャットをすることができるサービスです。特定のトピックや興味を共有する相手を探すこともできます。匿名性を保つこともできます。友達を作ったり、新しい出会いを楽しむことができます。</p>
+      <p class="text-center">
+        <NuxtLink class="btn btn-primary" to="/chat/new">今すぐマッチング開始</NuxtLink>
+      </p>
+    </div>
   </div>
 </template>
